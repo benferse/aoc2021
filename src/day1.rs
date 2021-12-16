033a4cdf533fd1efd3a37d29897024fe9eea86f2
Ben Fersenheim <ben@bu.edu>
2021-12-15 21:33:29 -0500

    Added solution for puzzle 1-2

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,5 @@
 use std::cmp::PartialOrd;
+use std::ops::Add;
 
 /// Given a sequence of measurements, count the number of times
 /// that the measurement increases from the previous measurement.
@@ -18,17 +19,48 @@ pub fn count_measurements<T>(measurements: &[T]) -> usize where T: PartialOrd {
         .count()
 }
 
+/// Given a sequence of measurements, count the number of times
+/// that the sum of subsequent sliding three-element windows is
+/// strictly increasing.
+///
+/// # Example s
+/// ```
+/// use aoc2021::day1::count_triples;
+/// let sample = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+/// assert_eq!(count_triples(&sample), 5);
+/// ```
+pub fn count_triples<T>(measurements: &[T]) -> usize where T: PartialOrd + Copy + Add<Output=T> {
+    let triplet_sums: Vec<T> = measurements
+        .windows(3)
+        .map(|a| a[0] + a[1] + a[2])
+        .collect();
+
+    count_measurements(&triplet_sums)
+}
+
 #[cfg(test)]
 mod answers {
     use super::*;
+    use once_cell::sync::OnceCell;
+
+    static INPUT: OnceCell<Vec<u32>> = OnceCell::new();
+
+    fn get_input() -> &'static Vec<u32> {
+        INPUT.get_or_init(|| {
+            include_str!("./input/day1")
+                .split_ascii_whitespace()
+                .filter_map(|line| line.parse().ok())
+                .collect()
+        })
+    }
 
     #[test]
     fn puzzle1() {
-        let input = include_str!("./input/day1")
-            .split_whitespace()
-            .filter_map(|s| s.parse::<u32>().ok())
-            .collect::<Vec<_>>();
+        println!("Puzzle answer is {}", count_measurements(get_input()));
+    }
 
-        println!("Puzzle answer is {}", count_measurements(&input));
+    #[test]
+    fn puzzle2() {
+        println!("Puzzle answer is {}", count_triples(get_input()));
     }
 }
